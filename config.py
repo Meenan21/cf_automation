65072beff38c7b1e477422a921753fa973cef928
Meenan21 <80931582+Meenan21@users.noreply.github.com>
2021-06-08 11:38:02 +1000

    comments added

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
 #App Config
+
+#NAme:codefresh_automation
+Repo - Access yaml / template / codefresh.yaml (clone api repo) / dockerfile / preperties file template
+
+API Repo - Source code (to pull - access req.Role) 
+
+
+
+Folder:
+  - Code (pkg)
+  - template (deploy)
+  - Env (dev/stga/pre)
+  - Config (properties)
+   
+#docker file  - Pkg name/ env / properties file (version)
+# access Control
